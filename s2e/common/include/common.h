e8a5b1a8d6b96505c65767660ea612217ecd9b6a
Karim BEN YOUSSEF <karim.benyoussef@gmail.com>
2026-05-21 16:16:10 +0200

    version update -> 1.5.0 Develop

--- s2e/common/include/common.h
+++ s2e/common/include/common.h
@@ -8,11 +8,11 @@
 //////////////////////////////////
 /* Application Firmware Version */
 #define MAJOR_VER               1
-#define MINOR_VER               4
-#define MAINTENANCE_VER         5
+#define MINOR_VER               5
+#define MAINTENANCE_VER         0
 
 //#define STR_VERSION_STATUS      //"Final"//"Develop" // or "Stable"
-#define STR_VERSION_STATUS      "Stable"
+#define STR_VERSION_STATUS      "Develop"
 
 //////////////////////////////////
 // W7500x HW Socket Definition  //
